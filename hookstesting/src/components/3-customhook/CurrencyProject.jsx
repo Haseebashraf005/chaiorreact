3e5face6c1024802c4cc903c45b231400a6e9fc2
haseeb <haseeb.ashraf@psca.gop.pk>
2024-07-25 18:02:36 +0500

    currency project last 10 mints

--- hookstesting/src/components/3-customhook/CurrencyProject.jsx
+++ hookstesting/src/components/3-customhook/CurrencyProject.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useCurrencyInfo from './components/hook/useCurrencyInfo'
+import Input from './components/Input'
 
 const CurrencyProject = () => {
 
-    let price =useCurrencyInfo("pkr")
-    console.log(price)
+    let [amount ,setAmount] = useState(0) 
+    let [from,setFrom] = useState("usd")
+    let [to,setTo] = useState("pkr")
+    let [convertedAmount,setConvertedAmount] = useState(0)
+    
 
+    let currencyInfo =useCurrencyInfo(from) 
+    let currencyOptions = Object.keys(currencyInfo)
+
+    console.log(currencyInfo)
+    // console.log(onlykey)
+ 
   
     
     return (
         <>
 
-        <h1 className='text-4xl text-center bg-orange-600 font-bold p-4 m-4'>testing</h1>
+        <h1 className='text-4xl text-center bg-orange-600 rounded font-bold p-4 m-4'>Custom Hook</h1>
+
+        <Input/>
 
         </>
 
