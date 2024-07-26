c56dc11c7151e8bac7628ed31f3c973d31ec8376
haseeb <haseeb.ashraf@psca.gop.pk>
2024-07-26 18:39:52 +0500

    day end currency friday

--- hookstesting/src/components/3-customhook/CurrencyProject.jsx
+++ hookstesting/src/components/3-customhook/CurrencyProject.jsx
@@ -1,34 +1,84 @@
-import React, { useState } from 'react'
-import useCurrencyInfo from './components/hook/useCurrencyInfo'
-import Input from './components/Input'
+import React, { useState } from "react";
+import useCurrencyInfo from "./components/hook/useCurrencyInfo";
+import Input from "./components/Input";
 
 const CurrencyProject = () => {
+    let [amount, setAmount] = useState(0);
+    let [from, setFrom] = useState("usd");
+    let [to, setTo] = useState("pkr");
+    let [convertedAmount, setConvertedAmount] = useState(0);
 
-    let [amount ,setAmount] = useState(0) 
-    let [from,setFrom] = useState("usd")
-    let [to,setTo] = useState("pkr")
-    let [convertedAmount,setConvertedAmount] = useState(0)
-    
+    let currencyInfo = useCurrencyInfo(from);
+    let currencyOptions = Object.keys(currencyInfo);
 
-    let currencyInfo =useCurrencyInfo(from) 
-    let currencyOptions = Object.keys(currencyInfo)
+    // swaping the variables
+    let swap = () => {
+        setFrom(to);
+        setTo(from);
+        setConvertedAmount(amount);
+        setAmount(convertedAmount);
+    };
 
-    console.log(currencyInfo)
+    let convert = () => {
+        setConvertedAmount(amount * currencyInfo[to]);
+    };
+
+    console.log(currencyInfo);
     // console.log(onlykey)
- 
-  
-    
+
     return (
-        <>
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert()
+                        }} 
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                            label="From"
+                            amount={amount}
+                            currencyOptions={currencyOptions}
+                            onCurrencyChange={(currency)=>setAmount(amount)}
+                            selectCurrency={from}
+                            
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                        <InputBox
+                            label="To"
+                            amount={convertedAmount  }
+                            currencyOptions={currencyOptions}
+                            onCurrencyChange={(currency)=>setTo(currency)}
+                            selectCurrency={from}
+                            
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+                        >
+                            Convert
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-        <h1 className='text-4xl text-center bg-orange-600 rounded font-bold p-4 m-4'>Custom Hook</h1>
-
-        <Input/>
-
-        </>
-
-
-    )
-}
-
-export default CurrencyProject
+export default CurrencyProject;
